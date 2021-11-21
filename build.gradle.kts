4dc4f0b14f113f75fb01aa4dc0b78a29b9575acd
kongkdh <kongkdh@kong-tech.com>
2021-11-21 19:29:18 +0900

    feat: swagger 문서 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     compileOnly("org.projectlombok:lombok")
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
     runtimeOnly("mysql:mysql-connector-java")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/com/wau/wau/api/rest/controller/RecordController.kt
+++ src/main/kotlin/com/wau/wau/api/rest/controller/RecordController.kt
@@ -4,6 +4,7 @@ import com.wau.wau.api.rest.model.request.RequestRecord
 import com.wau.wau.api.rest.model.response.ResponseRecord
 import com.wau.wau.api.rest.service.RecordService
 import com.wau.wau.common.model.response.FileUploadResponse
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile
 class RecordController(
     private val  recordService: RecordService
 ) {
+    @ApiOperation(value = "공유하고자하는 장소의 정보를 입력해주세요", notes = "공유하고자하는 장소의 정보를 입력해주세요")
     @PostMapping
     fun saveRecord(
         @RequestBody  requestRecord: RequestRecord
@@ -21,12 +23,13 @@ class RecordController(
         return ResponseEntity.ok(response)
     }
 
+    @ApiOperation(value = "기록된 모든 정보를 불러온다.", notes = "기록된 모든 정보를 불러온다.")
     @GetMapping("/get-all")
     fun  getRecords():ResponseEntity<List<ResponseRecord>>{
         val response : List<ResponseRecord> = recordService.getRecordList()
         return ResponseEntity.ok(response)
     }
-
+    @ApiOperation(value = "특정 사용자의 기록을 불러온다.", notes = "특정 사용자의 기록을 불러온다.")
     @GetMapping("/get/{userId}")
     fun getRecordByUserId(
         @PathVariable userId:String
@@ -34,7 +37,7 @@ class RecordController(
         val response: List<ResponseRecord> = recordService.getRecordByUserIdList(userId)
         return ResponseEntity.ok(response)
     }
-
+    @ApiOperation(value = "특정 기록을 삭제한다.", notes = "특정 기록을 삭제한다.")
     @DeleteMapping("/delete/{recordId}")
     fun deleteRecord(
         @PathVariable recordId: Int
@@ -42,7 +45,7 @@ class RecordController(
         val response: Boolean = recordService.deleteRecord(recordId)
         return ResponseEntity.ok(response)
     }
-
+    @ApiOperation(value = "이미지를 업로드한다.", notes = "이미지를 업로드한다.")
     @PostMapping("/post/images")
     fun uploadImages(
         @RequestParam("images", required = true) images: List<MultipartFile>,
